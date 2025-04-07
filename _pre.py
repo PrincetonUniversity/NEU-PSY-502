d0aacc5cb3f7514eced312f981f002aa76909d3f
Younes Strittmatter <younes_strittmatter@brown.edu>
2025-04-07 13:03:01 -0400

    add isc-ci scaffold

--- _pre.py
+++ _pre.py
@@ -122,7 +122,7 @@ def process_notebook(path=BUILD_DIR):
 
     with open(path, 'r', encoding='utf-8') as f:
         try:
-            notebook = nbformat.read(f, as_version=nbformat.NO_CONVERT)
+            notebook = nbformat.read(f, as_version=nbformat.current_nbformat)
         except Exception as e:
             print(f"Error reading notebook: {path}")
             print(e)
